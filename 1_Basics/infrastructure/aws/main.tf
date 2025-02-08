32aeb7f35899b7bfc52ba6bbe8261f473fcd86f8
saikrishna <saikrishna.nagisetti@gmail.com>
2025-02-08 16:58:47 +0800

    Initial commit

---
+++ 1_Basics/infrastructure/aws/main.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  region = var.region
+  profile = var.profile
+}
+
+# Create VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+# Create Public Subnets
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+}
+
+# Allocate Elastic IP for NAT Gateway (using first public subnet)
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+# Create NAT Gateway in the first public subnet
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+# Create Private Subnets
+resource "aws_subnet" "private" {
+  count      = length(var.private_subnets)
+  vpc_id     = aws_vpc.eks_vpc.id
+  cidr_block = var.private_subnets[count.index]
+}
+
+# Public Route Table with Internet Gateway
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Private Route Table with NAT Gateway for outbound internet access
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+}
+
+# Associate Private Subnets with Private Route Table
+resource "aws_route_table_association" "private_assoc" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
---
+++ 1_Basics/infrastructure/aws/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "profile" {
+  description = "AWS profile to use"
+  type        = string
+  
+}
